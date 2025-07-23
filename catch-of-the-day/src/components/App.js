90f9d3eb96077b7aa486dbba0342a36231fd112e
Anna Ossowska <anka.b.bogacka@gmail.com>
2025-07-23 21:20:44 +0200

    Removing and animation

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -53,7 +53,16 @@ class App extends React.Component {
         fishes[key] = updatedFish;
         // 3. Set that to state
         this.setState({ fishes });
-    }
+    };
+
+    deleteFish = (key) => {
+        // 1. Tale a copy of state
+        const fishes = { ...this.state.fishes };
+        // 2. Remove the state 
+        fishes[key] = null;
+        // 3. Update state
+        this.setState({ fishes });
+    };
 
     loadSampleFishes = () => {
         this.setState({ fishes: sampleFishes});
@@ -68,6 +77,15 @@ class App extends React.Component {
         this.setState({ order });
     };
 
+    removeFromOrder = (key) => {
+       // 1. Take a copy of state
+        const order = { ...this.state.order };
+        // 2. Remove from order 
+       delete order[key];
+        // 3. Call set state to update our state object
+        this.setState({ order }); 
+    }
+
     render() {
         return (
             <div className="catch-of-the-day">
@@ -79,10 +97,15 @@ class App extends React.Component {
                         ))}
                     </ul>
                 </div>
-                <Order fishes={this.state.fishes} order={this.state.order} />
+                <Order 
+                    fishes={this.state.fishes} 
+                    order={this.state.order} 
+                    removeFromOrder={this.removeFromOrder}
+                />
                 <Inventory 
                     addFish={this.addFish} 
                     updateFish={this.updateFish}
+                    deleteFish={this.deleteFish}
                     loadSampleFishes={this.loadSampleFishes}
                     fishes={this.state.fishes}
                 />
